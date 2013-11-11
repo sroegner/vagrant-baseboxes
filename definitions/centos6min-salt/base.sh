c985f445e843d6e34d21c41ebf9a629e5697ea96
Steffen Roegner <steffen@sroegner.org>
2013-11-11 09:55:15 -0500

    added some more packages

--- definitions/centos6min-salt/base.sh
+++ definitions/centos6min-salt/base.sh
@@ -3,7 +3,7 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils yum-utils tree sysstat strace tcpdump nmap atop python-pip git
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils yum-utils tree sysstat strace tcpdump nmap atop python-pip git redhat-lsb-core ntp
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
