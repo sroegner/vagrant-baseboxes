8759017b600a54a84e62f4f2ebfb00e3f6eb6c6d
Steffen Roegner <steffen.roegner@gmail.com>
2014-10-29 16:00:54 -0400

    add argparse and docker

--- definitions/centos6min-salt/salt.sh
+++ definitions/centos6min-salt/salt.sh
@@ -1,6 +1,6 @@
-
-yum -y install python-devel salt-master salt-minion python-halite --enablerepo epel-testing
-pip install gitdb GitPython
+yum -y install python-devel python-argparse salt-master salt-minion python-halite --enablerepo epel-testing
+pip install gitdb GitPython 
+#salt==2014.7.0rc4
 
 for svc in master minion syndic
 do
